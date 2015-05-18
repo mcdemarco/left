4475d2372c6b8f6442ff500898555f1443ffabb5
M. C. DeMarco <mcd@mcdemarco.net>
2015-05-18 11:57:37 -0400

    Add 5x5 bookmarklet.

--- tools/colourlets/5x5.js
+++ tools/colourlets/5x5.js
@@ -1,12 +1,16 @@
 javascript:(function() {
 	var badgeSize = 200;
+	//Get random colors.
 	var colorElements = document.getElementsByClassName("color");
-	if (colorElements.length > 4) {
+	if (document.getElementById("paletteDesc") && window.localStorage.fiveBadge) {
+		document.getElementById("paletteDesc").value = window.localStorage.fiveBadge;
+	} else if (colorElements.length > 4) {
 		var colors = [];
 		for (var co=0; co < colorElements.length; co++) {
 			if (colorElements[co].href && colorElements[co].href.indexOf("color/") > 0) {
-				if (colorElements[co].href.split("color/")[1].split("/")[0].length == 6)
-					colors.push(colorElements[co].href.split("color/")[1].split("/")[0]);
+				var candidate = colorElements[co].href.split("color/")[1].split("/")[0];
+				if (candidate.length == 6 && colors.indexOf(candidate) == -1)
+					colors.push(candidate);
 			}
 		}
 		if (colors.length < 4) {
@@ -24,9 +28,38 @@ javascript:(function() {
 		for (var i=0; i < 5; i++) {
 			cc[i] = new Array(5);
 		}
+		draw();
+		
+		document.getElementById("fiveUI").innerHTML = "<br style=\"clear:both\" />";
+		for (var i=0;i<4;i++) {
+			document.getElementById("fiveUI").innerHTML += "<input type=\"text\" onchange=\"fiveRedraw(" + i + ")\" size=5 id=\"fiveC" + i + "\" value=\"" + colors[i] + "\"/>";
+			if (i==1) document.getElementById("fiveUI").innerHTML += "<br/>";
+		}
+			
+	} else {
+		alert("To use this bookmarklet, you should be on a COLOURlovers page with more colors on it.");
+	}
+
+	window.fiveRedraw = function(changedColor) { 
+		colors[changedColor] = document.getElementById("fiveC" + changedColor).value;
+		draw();
+	};
+
+	window.fivePublish = function() {
+		for (var d = 0; d < 5; d++) {
+			var openString = "http://www.colourlovers.com/palettes/add?colors=";
+			for (var c = 0; c < 5; c++) {
+				openString += cc[c][d] + (c < 4 ? "," : "");
+			}
+			window.open(openString);
+		}
+	};
+
+	function draw() {
+		//Make 5x5 from colors.
 		cc[0][0] = colors[0];
-		cc[0][4] = colors[1];
-		cc[4][0] = colors[2];
+		cc[0][4] = colors[2];
+		cc[4][0] = colors[1];
 		cc[4][4] = colors[3];
 		var c, d, right, left, c1, c2, badge;
 		for (c = 0; c < 5; c++) {
@@ -38,18 +71,18 @@ javascript:(function() {
 			c1 = cc[4][0];
 			c2 = cc[4][4];
 			cc[4][c] = ("0" + Math.round(parseInt(c1.substring(0, 2), 16) * left + parseInt(c2.substring(0, 2), 16) * right).toString(16)).slice(-2) + ("0" + Math.round(((parseInt(c1.substring(2, 4), 16) * left + parseInt(c2.substring(2, 4), 16) * right))).toString(16)).slice(-2) + ("0" + Math.round(((parseInt(c1.substring(4, 6), 16) * left + parseInt(c2.substring(4, 6), 16) * right))).toString(16)).slice(-2);
-		}
-		badge = "<div>";
+			}
+		badge = "<div style=\"width:" + badgeSize + "px;\">";
 		var badgetteSize = badgeSize/5;
 		for (d = 0; d < 5; d++) {
-			badge += "<div>";
+			badge += "<div style=\"width:" + badgeSize + "px;\">";
 			for (c = 0; c < 5; c++) {
 				right = c/4;
 				left = (4 - c)/4;
 				c1 = cc[0][d];
 				c2 = cc[4][d];
 				cc[c][d] = ("0" + Math.round(parseInt(c1.substring(0, 2), 16) * left + parseInt(c2.substring(0, 2), 16) * right).toString(16)).slice(-2) + ("0" + Math.round(((parseInt(c1.substring(2, 4), 16) * left + parseInt(c2.substring(2, 4), 16) * right))).toString(16)).slice(-2) + ("0" + Math.round(((parseInt(c1.substring(4, 6), 16) * left + parseInt(c2.substring(4, 6), 16) * right))).toString(16)).slice(-2);
-				badge += "<div style=\"display:inline-block;width:" + badgetteSize + "px;height:" + badgetteSize + "px;background-color:#" + cc[c][d] + "\"></div>";
+				badge += "<div class=\"pattern\" style=\"display:inline-block;width:" + badgetteSize + "px;height:" + badgetteSize + "px;background-color:#" + cc[c][d] + "\"></div>";
 			}
 			badge += "</div>";
 		}
@@ -57,12 +90,15 @@ javascript:(function() {
 		try {
 			window.localStorage.fiveBadge = badge;
 		} catch (e) {};
-		var overlayDiv = document.createElement("div");
-		overlayDiv.style.cssText = "position:absolute;width:100%;height:100%;top:0;left:0;background-color:rgba(0,0,0,0.3);z-index:100;";
-		overlayDiv.innerHTML = "<div style=\"text-align:center;margin:10px;\">" + badge + "</div>";
-		document.body.appendChild(overlayDiv);
-	} else {
-		alert("To use this bookmarklet, you should be on a COLOURlovers page with more colors on it.");
+		
+		//Write the UI.
+		if (!document.getElementById("fiveOverlay")) {
+			var overlayDiv = document.createElement("div");
+			overlayDiv.style.cssText = "position:absolute;width:100%;height:100%;top:0;left:0;background-color:rgba(0,0,0,0.3);z-index:100;overflow:hidden;";
+			overlayDiv.innerHTML = "<div id=\"fiveOverlay\" style=\"margin:10px;\"><div id=\"fiveBadge\"></div><div id=\"fiveUI\"></div><div><button type=\"button\" style=\"background-color:white;border-radius:5px;padding:0 5px;\" onclick=\"fivePublish();\">Publish</button> <button type=\"button\" style=\"background-color:white;border-radius:5px;padding:0 5px;\" onclick=\"this.parentElement.parentElement.parentElement.remove();\">Close</button></div></div>";
+			document.body.appendChild(overlayDiv);
+		}
+		document.getElementById("fiveBadge").innerHTML = badge;
 	}
+	
 })();
-
