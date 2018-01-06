b972fd59aa0e7d13024c133caccb0a8495315da5
M. C. DeMarco <>
2018-01-06 17:45:17 -0500

    Improve sorter.

---
+++ games/bgg/sort.js
@@ -0,0 +1,137 @@
+//
+//fetch a geeklist with a cors proxy; sort and display it with xslt (so oldskool!) 
+//
+
+(function () {
+	//var geeklistId = 234925;
+	var geeklistURL = "https://boardgamegeek.com/xmlapi/geeklist/";
+	var geeklistStatus = {};
+	var minutes = 10; //Don't repeat successful requests within 10 min.
+	//The api doesn't always respond with the goods.
+	var waitMessage = "Your request for this geeklist has been accepted and will be processed.";
+	//Requires a proxy because the BGG API is broken in yet another way.
+	//wore this one out once, but it's back: 
+	var corsProxy = "https://cors-anywhere.herokuapp.com/";
+	//var corsProxy = "https://galvanize-cors-proxy.herokuapp.com/";
+	//Local xsl.
+	var stylesheetURL = "sort.xsl";
+	var stylesheet;
+
+	function requestStylesheet(stylesheetURL) {
+		//Fetch stylesheet.
+		var sReq = new XMLHttpRequest();
+		sReq.addEventListener("load", sReqListener);
+		sReq.open("GET", stylesheetURL);
+		sReq.send();
+	}
+
+	function sReqListener () {
+		stylesheet = this.responseXML;
+	}
+	
+	function requestGeeklist(geeklistId,comments) {
+		var oReq = new XMLHttpRequest();
+		oReq.addEventListener("load", reqListener);
+		oReq.open("GET", corsProxy + geeklistURL + geeklistId + (comments ? "?comments=1" : ""));
+		oReq.send();
+	}
+	
+	function reqListener() {
+		var geeklistXML = this.responseXML;
+		//Often the response is "wait a minute"; 
+		//the stylesheet will display that, but we still want to know.
+		if (geeklistXML.firstChild.nodeName == "geeklist") {
+			//This is worth saving.
+			geeklistStatus.date = new Date();
+			geeklistStatus.xml = geeklistXML;
+			geeklistStatus.id = parseInt(geeklistXML.firstChild.getAttribute("id"),10);
+			geeklistStatus.comments = document.getElementById("comments").checked;
+		}
+		transformAndWrite(geeklistXML);
+	}
+
+	function transformAndWrite(geeklistXML) {
+		var fragment = transform(geeklistXML,stylesheet);
+		document.getElementById("geeklist").appendChild(fragment);
+	}
+
+	function transform(geeklist,stylesheet) {
+		var xmlDom;
+		var sortBy = document.getElementById("sortBy").value;
+		var ascending = document.getElementById("ascending").checked;
+		var images = document.getElementById("images").checked;
+		var comments = document.getElementById("comments").checked;
+		if (typeof XSLTProcessor == "undefined") {
+			try {
+				xmlDom = geeklist.transformNode(stylesheet);
+			} catch(e) {
+				xmlDom = "An error occurred (" + e.description + ").";
+			}
+		} else { //webkit
+			var xsltProcessor = new XSLTProcessor();
+			xsltProcessor.setParameter(null, "sortby", sortBy);
+			xsltProcessor.setParameter(null, "ascending", ascending);
+			xsltProcessor.setParameter(null, "images", images);
+			xsltProcessor.setParameter(null, "comments", comments);
+			xsltProcessor.importStylesheet(stylesheet);
+			xmlDom = xsltProcessor.transformToFragment(geeklist, document);
+		}
+		return xmlDom;
+	}
+	
+	function getGeekli() {
+		var geeklistId = parseID(document.getElementById("geeklistIdINPUT").value);
+		if (geeklistId == 0)
+			alert("Bad geeklist id or URL!");
+		else {
+			var comments = document.getElementById("comments").checked;
+			document.getElementById("geeklist").innerHTML = "";
+			//Decide whether to make a new request.  
+			//Need a new one for a new ID (duh), added comments, or time lapses (in min).
+			if (geeklistStatus.id && 
+					geeklistStatus.id == geeklistId &&
+					(geeklistStatus.comments || !comments) &&
+					new Date() - geeklistStatus.date < 60000 * minutes) {
+				//Re-transform the old data.
+				transformAndWrite(geeklistStatus.xml);
+			} else {
+				//Fetch new data.
+				requestGeeklist(geeklistId,comments);
+			}
+		}
+	}
+	
+	function parseID(protoId) {
+		if ((protoId.split("geeklist/")).length > 1)
+			protoId = protoId.split("geeklist/")[1].split("/")[0];
+		
+		if (parseInt(protoId,10) > 0)
+			return parseInt(protoId,10);
+		else 
+			return 0;
+	}
+	
+	function setFromQuery() {
+		if (window.location.search && parseInt(window.location.search.split("?")[1],10) > 0) {
+			document.getElementById("geeklistIdINPUT").value = parseInt(window.location.search.split("?")[1],10);
+			//also autoload.
+			getGeekli();
+		}
+	}
+	
+	/* onload */
+	function loady() {
+		//Don't need to wait for load for the stylesheet, but for the others.
+		requestStylesheet(stylesheetURL);
+		document.getElementsByTagName("form")[0].addEventListener("submit", function(e) {
+			e.preventDefault();
+			getGeekli();
+			return false;
+		});
+		setFromQuery();
+		document.getElementById("urlHint").innerHTML = [location.protocol, '//', location.host, location.pathname, '?234925'].join('');
+	}
+	
+	window.onload = loady;
+
+})();
--- games/bgg/sort.xsl
+++ games/bgg/sort.xsl
@@ -3,22 +3,42 @@
 	<xsl:param name="sortby">date</xsl:param>
 	<xsl:param name="ascending">false</xsl:param>
 	<xsl:param name="images">true</xsl:param>
+	<xsl:param name="comments">false</xsl:param>
 <xsl:template match="/">
 	<xsl:choose>
 		<xsl:when test="//message">
 			<!-- show any messages -->
-			<p style="color:tomato;"><xsl:value-of select="//message" /></p>
+			<p class="message"><xsl:value-of select="//message" /></p>
 			<p>(This probably means you should wait a minute and click Sort again.)</p>
 		</xsl:when>
 		<xsl:otherwise>
 			<div id="header" class="entry">
 				<h2><a href="https://boardgamegeek.com/geeklist/{//geeklist/@id}/"><xsl:value-of select="//geeklist/title" /></a></h2>
 				<div class="entrycontents">
-					<cite>Posted: <xsl:value-of select="substring(//geeklist/postdate,1,22)" />
-					<xsl:if test = "//geeklist/postdate != //geeklist/editdate">
-						<br/>Edited: <xsl:value-of select="substring(//geeklist/editdate,1,22)" />
-					</xsl:if>
+					<cite>
+						Posted: 
+						<xsl:value-of select="substring(//geeklist/postdate,1,22)" />
+						<xsl:if test = "//geeklist/postdate != //geeklist/editdate">
+							<br/>Edited: <xsl:value-of select="substring(//geeklist/editdate,1,22)" />
+						</xsl:if>
+						<xsl:if test="$comments='true' and //geeklist/comment">
+							<br/>Latest comment: <xsl:value-of select="substring((//geeklist/comment)[last()]/@postdate,1,22)"/>
+						</xsl:if>
 					</cite>
+					<div>
+						<cite>Created by <xsl:value-of select="//username" /></cite><br/>
+						<xsl:call-template name="pluralizer">
+							<xsl:with-param name="theCount" select="//thumbs"/>
+							<xsl:with-param name="theWord" select="'thumb'"/>
+						</xsl:call-template>
+						<xsl:if test="$comments='true'">
+							<br/>
+							<xsl:call-template name="pluralizer">
+								<xsl:with-param name="theCount" select="count(//geeklist/comment)"/>
+								<xsl:with-param name="theWord" select="'comment'" />
+							</xsl:call-template>
+						</xsl:if>
+					</div>
 				</div>
 			</div>
 			<xsl:choose>
@@ -102,20 +122,44 @@
 				</a>			
 			</h3>
 			<div class="entrycontents">
-				<xsl:if test="@imageid and @imageid &gt; 0 and $images = 'true'">
-					<img src="https://cf.geekdo-images.com/images/pic{@imageid}_t.jpg" />
-				</xsl:if>
+				<div>
+					<xsl:if test="@imageid and @imageid &gt; 0 and $images = 'true'">
+						<img alt="" src="https://cf.geekdo-images.com/images/pic{@imageid}_t.jpg" 
+						     onerror="this.src='https://cf.geekdo-images.com/images/pic{@imageid}_t.png'" />
+					</xsl:if>
+				</div>
 				<div>
 					Posted: <xsl:value-of select="substring(@postdate,1,22)" /><br />
 					<xsl:if test = "@postdate != @editdate">
 						Edited: <xsl:value-of select="substring(@editdate,1,22)" /><br />
 					</xsl:if>
-					<cite>Added by <xsl:value-of select="@username" /></cite><br />
-					<xsl:value-of select="@thumbs" /> thumbs
+					<cite>Added by <xsl:value-of select="@username" /></cite>
+					<br />
+					<xsl:call-template name="pluralizer">
+						<xsl:with-param name="theCount" select="@thumbs"/>
+						<xsl:with-param name="theWord" select="'thumb'"/>
+					</xsl:call-template>
 					<xsl:if test = "$sortby = 'type'"><br /><xsl:value-of select="@subtype" /></xsl:if>
+					<xsl:if test="$comments='true'">
+						<br/>
+						<xsl:call-template name="pluralizer">
+							<xsl:with-param name="theCount" select="count(comment)"/>
+							<xsl:with-param name="theWord" select="'comment'"/>
+						</xsl:call-template>
+						<xsl:if test="$comments='true' and comment">
+							<br/>Latest comment: <xsl:value-of select="substring((comment)[last()]/@postdate,1,22)"/>
+						</xsl:if>
+					</xsl:if>
 				</div>
 			</div>
 		</div>
+	</xsl:template>
+
+	<xsl:template name="pluralizer">
+		<xsl:param name="theCount"/>
+		<xsl:param name="theWord"/>
+		<xsl:value-of select="$theCount"/><xsl:text> </xsl:text>
+		<xsl:value-of select="$theWord"/><xsl:if test="not($theCount = 1)">s</xsl:if>
 	</xsl:template>
 
 </xsl:stylesheet>
