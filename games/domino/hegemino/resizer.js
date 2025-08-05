3262ce634b0a5e2db554da5f40afc2b214bbf515
M. C. DeMarco <mcd@mcdemarco.net>
2025-08-05 12:00:01 -0400

    Framify the scoresheets

---
+++ games/domino/hegemino/resizer.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 6 */
+//Resizes iframes and checks dorothy if present.
+
+function resizeOnLoad(frame) {
+	frame.height = frame.contentDocument.body.parentElement.scrollHeight;
+	if (frame.id == "dorothy")
+	  frame.contentDocument.getElementById("dorothy").checked = true;
+}
+
+var iframes = document.getElementsByTagName("iframe");
+for (var iframe of iframes) {
+	iframe.setAttribute("onload", "resizeOnLoad(this)");
+	iframe.src = "/games/domino/hegemino/sheet.html";
+}
